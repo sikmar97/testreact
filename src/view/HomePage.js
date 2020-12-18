62a39d8e883da957b8e3622213e692c6f6ab1a7a
sikke <sikmar35@gmail.com>
2020-12-18 19:55:50 +0100

    added more to useEffect

--- src/view/HomePage.js
+++ src/view/HomePage.js
@@ -1,11 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 export const HomePage = () => {
-
-    
-    
+   const [rerender, setRerender] = useState(0)
+   
+    useEffect(() => { 
+            alert("Component Renderd")
+        return () => {
+            alert("Component Removed")
+        }
+    })
+       
     return (
         <div>
             <h1>Welcome to my page!</h1>
+            <button onClick={()=> setRerender(rerender + 1)}>Click To Render Component</button>
+
+            <h1>Component Has Been Renderd {rerender} Times</h1>
         </div>
     )
 }
